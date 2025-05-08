d827c4ba1c7ac3f02843d47c9ab7ca1056c12774
Tipto_Ghosh <147062709+Tipto-Ghosh@users.noreply.github.com>
2025-05-08 10:01:59 +0600

    Updating leetcode POTD May-07 , 2025

--- Leetcode Daily Challenges/May25/day_07.py
+++ Leetcode Daily Challenges/May25/day_07.py
@@ -1,39 +1,59 @@
-class State:
-    def __init__(self, x, y, dis):
-        self.x = x
-        self.y = y
-        self.dis = dis
-
-    def __lt__(self, other):
-        return self.dis < other.dis
-
+import heapq
 
 class Solution:
     def minTimeToReach(self, moveTime):
-        n = len(moveTime)
-        m = len(moveTime[0])
-        inf = float("inf")
-        d = [[inf] * m for _ in range(n)]
-        v = [[0] * m for _ in range(n)]
-
-        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        d[0][0] = 0
-        q = []
-        heapq.heappush(q, State(0, 0, 0))
-
-        while q:
-            s = heapq.heappop(q)
-            if v[s.x][s.y]:
+        """
+        1. Starting time = 0s and position is [0,0]
+        2. Moving from a to b we need max(time_a , time_b) + 1
+        3. Only we can move to left , right , up and down
+        
+        We need to find the shortest time from (0 , 0) to (rows-1 , cols - 1)
+        we will use shortest path to find the min time
+        """
+        infinity = float('inf')
+        directions = [(1 , 0) , (-1 , 0) , (0 , 1) , (0 , -1)]
+        
+        Rows , Cols = len(moveTime) , len(moveTime[0])
+        
+        # source
+        src = (0 , 0 , 0) # position [0 , 0] and src to src time 0
+        min_heap = []
+        
+        heapq.heappush(min_heap , src)
+        
+        visited = [[False] * Cols for _ in range(Rows)] # track the visited cells
+        # also make a dist list which store the shorted time from [0,0] to every cell
+        distance = [[infinity] * Cols for _ in range(Rows)] # initially all distance/time is inifinity
+        # src to src time is 0
+        distance[0][0] = 0
+        
+        while min_heap:
+            # take the cell
+            time , r , c = heapq.heappop(min_heap)
+            
+            # if currenct cell is visited then don't need to revisit , skip it
+            if visited[r][c]:
                 continue
-            v[s.x][s.y] = 1
-            for dx, dy in dirs:
-                nx, ny = s.x + dx, s.y + dy
-                if not (0 <= nx < n and 0 <= ny < m):
+            
+            # So make it visited
+            visited[r][c] = True
+            
+            # otherwise take the current cell and move to it's adjacent cells
+            for dr , dc in directions:
+                nr , nc = r + dr , c + dc 
+                # If not a valid cell then skip
+                if not (0 <= nr < Rows and 0 <= nc < Cols):
                     continue
-                dist = max(d[s.x][s.y], moveTime[nx][ny]) + 1
-                if d[nx][ny] > dist:
-                    d[nx][ny] = dist
-                    heapq.heappush(q, State(nx, ny, dist))
+                # find the time
+                time_needed = 1 + max(distance[r][c] , moveTime[nr][nc])
+                # if we got a smaller time than current time then update
+                if time_needed < distance[nr][nc]:
+                    distance[nr][nc] = time_needed
+                    heapq.heappush(min_heap , (time_needed , nr , nc))
+                
+            
+        return distance[Rows - 1][Cols - 1]
+        
+sol = Solution()
 
-        return d[n - 1][m - 1]
+print(sol.minTimeToReach([[0,4],[4,4]]))        
