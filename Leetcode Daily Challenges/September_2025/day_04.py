ec25714e061df35ec5359c59c252d2cb29d4313e
Tipto_Ghosh <tiptoghosh@gmail.com>
2025-09-04 18:43:11 -0500

    Leetcode POTD day-04 Sept-2025 added

--- Leetcode Daily Challenges/September_2025/day_04.py
+++ Leetcode Daily Challenges/September_2025/day_04.py
@@ -1,8 +1,15 @@
 class Solution:
     def findClosest(self, x: int, y: int, z: int) -> int:
-        """ 
-        Find the distance of both x and y from z.
-        And say which distance is lowest. 
+        """
+        Determines which of the two numbers (x or y) is closer to a third number (z).
+        
+        Args:
+            x (int): First number.
+            y (int): Second number.
+            z (int): Reference number to compare distances.
+        
+        Returns:
+            int: Returns 1 if x is closer to z, 2 if y is closer to z, and 0 if both are equally close.
         """
         dis_x = abs(x - z)
         dis_y = abs(y - z)
